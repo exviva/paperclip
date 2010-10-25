5a8572849827371c75ccc277bbb3692f2e770a9b
Hassan Khalid <h.scorpion.k@gmail.com>
2010-10-25 20:52:38 +0300

    Revert "Style#initialize - dup definition before deleting options"
    We first want to merge with upstream
    
    This reverts commit 39447de9fefc63c317672812118d479e78bfb46c.

--- lib/paperclip/style.rb
+++ lib/paperclip/style.rb
@@ -15,7 +15,6 @@ module Paperclip
       @name = name
       @attachment = attachment
       if definition.is_a? Hash
-        definition = definition.dup
         @geometry = definition.delete(:geometry)
         @format = definition.delete(:format)
         @processors = definition.delete(:processors)
